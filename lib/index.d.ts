8381f209823565995ddd389b1227883625f82859
Gerard del Castillo <gerarddelcastillo@gmail.com>
2020-04-19 09:57:36 +0200

    reexport on index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,13 @@
 import * as Chart from "./Chart";
 import * as RawChart from "./RawChart";
+import { ErrorObject } from "./Error";
 import { Failable } from "./Failable";
-export interface ParseError {
-    error: any;
-}
+export * as Meta from "./Meta";
+export * as Chart from "./Chart";
+export * as RawChart from "./RawChart";
+export * as Failable from "./Failable";
+export { ErrorObject } from "./Error";
 export default class Parser {
-    static parseRaw(text: string, semanticCheck?: boolean): Failable<RawChart.RawChart, ParseError>;
-    static parse(text: string): Failable<Chart.Chart, ParseError>;
+    static parseRaw(text: string, semanticCheck?: boolean): Failable<RawChart.RawChart, ErrorObject>;
+    static parse(text: string): Failable<Chart.Chart, ErrorObject>;
 }
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import grammar from "./grammar"
 import * as Meta 		from "./Meta"
 import * as Chart 		from "./Chart"
 import * as RawChart 	from "./RawChart"
+import {ErrorObject}	from "./Error"
 import {
 	Failable,
 	Failure,
@@ -15,11 +16,13 @@ import {
 
 import check from "./Semantic"
 
-export interface ParseError {
-	error: any
-}
+export * as Meta 		from "./Meta"
+export * as Chart 		from "./Chart"
+export * as RawChart 	from "./RawChart"
+export * as Failable 	from "./Failable"
+export {ErrorObject}	from "./Error"
 
-function executeParser(text: string): Failable<Meta.ParsedChart, ParseError> {
+function executeParser(text: string): Failable<Meta.ParsedChart, ErrorObject> {
 	const parser = new Nearley.Parser(
 		Nearley.Grammar.fromCompiled(grammar)
 	)
@@ -27,11 +30,11 @@ function executeParser(text: string): Failable<Meta.ParsedChart, ParseError> {
 	try {
 		parser.feed(text);
 	} catch (err) {
-		return Failure({error: err})
+		return Failure({reason: err})
 	}
 
 	if (parser.results.length < 1) {
-		return Failure({error: "Invalid Chart file"})
+		return Failure({reason: "Invalid Chart file"})
 	} else if(parser.results.length > 1) {
 		console.warn("chart2json: Ambiguous input")
 	}
@@ -39,7 +42,7 @@ function executeParser(text: string): Failable<Meta.ParsedChart, ParseError> {
 	return Success(parser.results[0])
 }
 
-function parseChart(text: string, semanticCheck?: boolean): Failable<Meta.ParsedChart, ParseError> {
+function parseChart(text: string, semanticCheck?: boolean): Failable<Meta.ParsedChart, ErrorObject> {
 	const shouldCheckSemantics = semanticCheck !== undefined ? semanticCheck : false
 	const parseResult = executeParser(text)
 	if (!parseResult.ok) {
@@ -50,7 +53,7 @@ function parseChart(text: string, semanticCheck?: boolean): Failable<Meta.Parsed
 	if (shouldCheckSemantics) {
 		const semanticError = check(parsedChart)
 		if (semanticError) {
-			return Failure({error: semanticError})
+			return Failure(semanticError)
 		}
 	}
 
@@ -58,7 +61,7 @@ function parseChart(text: string, semanticCheck?: boolean): Failable<Meta.Parsed
 }
 
 export default class Parser {
-	static parseRaw(text: string, semanticCheck?: boolean): Failable<RawChart.RawChart, ParseError> {
+	static parseRaw(text: string, semanticCheck?: boolean): Failable<RawChart.RawChart, ErrorObject> {
 		const parseResult = parseChart(text, semanticCheck)
 		if (!parseResult.ok) {
 			return parseResult
@@ -69,7 +72,7 @@ export default class Parser {
 		)
 	}
 
-	static parse(text: string): Failable<Chart.Chart, ParseError> {
+	static parse(text: string): Failable<Chart.Chart, ErrorObject> {
 		const parseResult = parseChart(text, true)
 		if (!parseResult.ok) {
 			return parseResult
